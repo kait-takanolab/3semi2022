7bd1688012562f1d18093705dd82dab77dc4fd0d
ktakano <takano@ic.kanagawa-it.ac.jp>
2022-06-01 12:35:24 +0900

    Added INSERT SQL sentence.

--- php/sentiment.php
+++ php/sentiment.php
@@ -6,6 +6,24 @@ $password='';
 try {
     $dbh=new PDO($dsn,$user,$password);
     print("DBに接続しました。<br>");
+
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+    # INSERT INTO sentiment (id,sentence,posinega,score) 
+    # VALUES (1, '私は犬が好きです。', 1, 0.95);
+
+    # プリペアドステートメント
+    $sql="insert into sentiment (sentence,posinega,score)" . 
+         " values (:sentence, :posinega, :score);";
+    
+    $stmt=$dbh->prepare($sql);
+    #$stmt->bindValue(':id', "2", PDO::PARAM_INT);
+    $stmt->bindValue(':sentence',  $_GET["usermsg"], PDO::PARAM_STR);
+    $stmt->bindValue(':posinega', 1, PDO::PARAM_INT);
+    $stmt->bindValue(':score', 0.98, PDO::PARAM_STR);
+    $stmt->execute();
+    echo '登録しました。';
+
 } catch(PDOException $e){
     print($e->getMessage());
     die();
