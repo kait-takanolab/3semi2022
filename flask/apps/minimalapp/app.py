6ff96330ac9794c99860f8b1f5113633ba04024b
ktakano <takano@ic.kanagawa-it.ac.jp>
2022-07-20 15:25:50 +0900

    Added routes, "sentiment" and "sentiment_do".

--- flask/apps/minimalapp/app.py
+++ flask/apps/minimalapp/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Flaskbook!"
+    return "Hello, Takanolab!"
+
+@app.route("/hello",
+  methods=["GET"],
+  endpoint="hello-endpoint")
+def hello():
+    return "Hello, KAIT!"
+
+@app.route("/sentiment",
+  methods=["GET"],
+  endpoint="sentiment")
+def sentimet():
+  #return "sentiment"
+  return render_template("index_py.html")
+
+@app.route("/sentiment_do",
+  methods=["GET"],
+  endpoint="sentiment_do")
+def sentiment_do():
+  # GETのときはrequest.args.getを使う
+  message = request.args.get('usermsg')
+  # return message
+  return render_template("result.html", message=message)
